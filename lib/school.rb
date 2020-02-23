bae57fe365e411409ad11c27b014c74121c0ca28
Frank  D'Angelo <08fdange@gmail.com>
2020-02-23 03:05:50 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+class School
+  def initialize(name)
+    @name = name
+  end
+  
+  def roster
+    roster = {}
+  end
+  
+  def add_student(student_name, grade)
+    roster[grade] = []
+    roster[grade] << student_name
+  end
+  
+  
+  
+end
