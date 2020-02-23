bed70aa4db74f08c7cfc5fb6b7cd4f2f56e5efb1
Frank  D'Angelo <08fdange@gmail.com>
2020-02-23 03:49:56 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -16,12 +16,6 @@ class School
   end
  
   def sort 
-    roster.each do |grade, students|
-      grade[students].
-    end
-  return roster
+    roster[grade][student_name].sort
   end
-   
-  
-
 end
